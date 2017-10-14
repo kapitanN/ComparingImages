950a743d326f83ee148480a8070d2e8884f47c1d
kapitanN <nikita.shapovalov.94@gmail.com>
2017-10-14 11:19:58 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        ImagesComparingService service = new ImagesComparingService("image1.jpg", "image2.jpg");
+        service.compareImagesAndDrawRectangles();
+        service.saveNewImage( "myImage.jpg");
+    }
+}
